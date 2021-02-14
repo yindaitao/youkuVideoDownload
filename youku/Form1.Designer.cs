7c59741c2f3fbc5278e6c898e30c567a1628dbba
Vincent <Vincent.yin@outlook.com>
2021-02-15 01:42:04 +0800

    添加项目文件。

---
+++ youku/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace youku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "视频地址";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(627, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "批量获取";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(12, 110);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtContent.Size = new System.Drawing.Size(776, 328);
+            this.txtContent.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(311, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(192, 34);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "手动下载";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(54, 18);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(269, 12);
+            this.linkLabel1.TabIndex = 11;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "在Chrome开发工具中查找地址中包含“m3u8”链接";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(509, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(192, 34);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "合并视频";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ youku/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace youku
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtContent.Text = "";
+                var response = http(textBox1.Text);
+                var stream = response.GetResponseStream();
+
+                var vl = new List<video>();
+                using (var sr = new StreamReader(stream))
+                {
+                    //var content = sr.ReadToEnd();
+                    //txtContent.Text = content;
+                    var n = 1;
+                    while (sr.Peek() >= 0)
+                    {
+                        var line = sr.ReadLine();
+                        if (line.StartsWith("#EXTINF"))
+                        {
+                            var timeStr = line.Split(':')[1].Split(',')[0];
+                            var url = sr.ReadLine();
+                            txtContent.Text += n.ToString() + "                                           " + timeStr + "\r\n";
+                            txtContent.Text += url + "\r\n\r\n";
+                            var v = new video
+                            {
+                                index = n,
+                                time = Convert.ToDecimal(timeStr),
+                                url = url
+                            };
+                            vl.Add(v);
+                            n++;
+                        }
+                    }
+                }
+                txtContent.Text = "共【" + vl.Count.ToString() + "】个视频文件\r\n" + txtContent.Text;
+                // 文件下载地址
+                var dir = new FolderBrowserDialog();
+                if (dir.ShowDialog() != DialogResult.OK) return;
+                string path = dir.SelectedPath;// "E:/文件下载";
+                                               // 如果不存在就创建file文件夹
+                if (!Directory.Exists(path))
+                {
+                    if (path != null) Directory.CreateDirectory(path);
+                }
+                foreach (var v in vl)
+                {
+                    var thread = new Thread(new ParameterizedThreadStart(download));
+                    thread.Start(new dt
+                    {
+                        url = v.url,
+                        fileName = path + "\\" + v.index + ".flv.ts"
+                    });
+                }
+
+                MessageBox.Show("Ok");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        class dt
+        {
+            public string url { get; set; }
+            public string fileName { get; set; }
+        }
+        private void download(object state)
+        {
+            try
+            {
+                var obj = state as dt;
+                var responseAvi = http(obj.url);
+                using (Stream streamAvi = responseAvi.GetResponseStream())
+                {
+                    using (var sos = new System.IO.FileStream(obj.fileName, System.IO.FileMode.Create))
+                    {
+                        byte[] img = new byte[1024];
+                        int total = streamAvi.Read(img, 0, img.Length);
+                        while (total > 0)
+                        {
+                            //之后再输出内容
+                            sos.Write(img, 0, total);
+                            total = streamAvi.Read(img, 0, img.Length);
+                        }
+                    }
+                }
+                Thread.Sleep(Convert.ToInt32(100));
+            }
+            catch
+            {
+
+            }
+        }
+        private System.Net.HttpWebResponse http(string url)
+        {
+            //请求网络路径地址
+            var request = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(url);
+            request.Timeout = 5000; // 超时时间
+                                    //获得请求结果
+            var response = (System.Net.HttpWebResponse)request.GetResponse();
+            return response;
+        }
+        class video
+        {
+            public int index { get; set; }
+            public decimal time { get; set; }
+            public string url { get; set; }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var responseAvi = http(textBox1.Text);
+                using (Stream streamAvi = responseAvi.GetResponseStream())
+                {
+                    var dialog = new SaveFileDialog();
+                    dialog.Filter = "*.flv.ts|*.flv.ts";
+                    if (dialog.ShowDialog() != DialogResult.OK) return;
+                    var path = dialog.FileName;
+                    using (var sos = new System.IO.FileStream(path, System.IO.FileMode.Create))
+                    {
+                        byte[] img = new byte[1024];
+                        int total = streamAvi.Read(img, 0, img.Length);
+                        while (total > 0)
+                        {
+                            //之后再输出内容
+                            sos.Write(img, 0, total);
+                            total = streamAvi.Read(img, 0, img.Length);
+                        }
+                    }
+                }
+                MessageBox.Show("Ok");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var dir = new FolderBrowserDialog();
+                if (dir.ShowDialog() != DialogResult.OK) return;
+
+                var fileNames = new StringBuilder();
+                foreach (string fileName in Directory.GetFiles(dir.SelectedPath))
+                {
+                    fileNames.Append(fileName + "?");
+                }
+                fileNames.Remove(checked(fileNames.Length - 1), 1);
+                Thread thread = new Thread(mergeVideo);
+                thread.Start(fileNames.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void mergeVideo(object obj)
+        {
+            mergeVideo((string)obj);
+        }
+
+        private void mergeVideo(string fileNames)
+        {
+            string[] names = fileNames.Split('?');
+            FileInfo fileInfo = new FileInfo(names[0]);
+            string outName = fileInfo.DirectoryName + "\\merge_" + fileInfo.Name;
+            outName = "\"" + outName + "\"";
+            string fileStr = "";
+            string[] array = names;
+            foreach (string name in array)
+            {
+                fileStr = fileStr + "file '" + name + "'\r\n";
+            }
+            File.WriteAllText("list.txt", fileStr);
+            string arguments = "-f concat -i list.txt -c copy " + outName;
+            runProcess("ffmpeg.exe", arguments);
+        }
+
+        private void runProcess(string processName, string arguments)
+        {
+            var p = new Process();
+            p.StartInfo.FileName = processName;
+            p.StartInfo.Arguments = arguments;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.ErrorDataReceived += ErrorDataReceived;
+            p.OutputDataReceived += OutputDataReceived;
+            p.StartInfo.UseShellExecute = false;
+            p.Start();
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            p.BeginErrorReadLine();
+            p.WaitForExit();
+            p.Close();
+            p.Dispose();
+        }
+
+        private void OutputDataReceived(object obj, DataReceivedEventArgs e)
+        {
+            //_message.AppendLine(e.Data);
+            //if (textBoxMessage.InvokeRequired)
+            //{
+            //    textBoxMessage.Invoke(new EventHandler(showMessage));
+            //}
+        }
+
+        private void ErrorDataReceived(object obj, DataReceivedEventArgs e)
+        {
+            //_message.AppendLine(e.Data);
+            //if (textBoxMessage.InvokeRequired)
+            //{
+            //    textBoxMessage.Invoke(new EventHandler(showMessage));
+            //}
+        }
+    }
+}
